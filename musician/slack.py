10a6769a1c6fade783f514e0059b1124af579ec6
손봉우 <bongudev@gmail.com>
2022-01-24 14:43:23 +0000

    diabled send_message

--- musician/slack.py
+++ musician/slack.py
@@ -70,7 +70,7 @@ class UpdateMusician:
         
         for album in self.__albums:
             headers = {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/json; charset=utf-8',
                 'Authorization': 'Bearer ' + config('SLACK_TOKEN')
             }
             msg = {
